f55d6910f77d53494dc70b1850d3c39069f2bdf5
k0rrluna <k0rr.luna1@gmail.com>
2024-06-04 14:43:22 +0300

    python

---
+++ converter.py
@@ -0,0 +1,34 @@
+def kbToB(kb):
+    b = 1
+    b = kb * 1024
+    return b
+
+def mToKm(m):
+    km = 1
+    km = m * 0.001
+    return km
+
+def gToKg(g):
+    kg = 1
+    kg = g * 0.001
+    return kg
+
+def sToMs(s):
+    ms = 1
+    ms = s *  1000
+    return ms
+
+def cToF(c):
+    f = 1
+    f = (c*9/5) + 32
+    return f
+
+def hzToKhz(hz):
+    khz = 1
+    khz = hz * 0.001
+    return khz
+
+def wToKw(w):
+    kw = 1
+    kw = w * 0.001
+    return kw
---
+++ lab6.py
@@ -0,0 +1,9 @@
+import converter
+
+print(converter.kbToB(5))
+print(converter.mToKm(1000))
+print(converter.gToKg(500))
+print(converter.sToMs(10))
+print(converter.cToF(25))
+print(converter.hzToKhz(1000))
+print(converter.wToKw(500))
